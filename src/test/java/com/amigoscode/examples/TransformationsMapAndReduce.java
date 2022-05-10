bc799d1580ba670c0771528090147a0a8598eec0
Esteban Gomez <unawaretub86@gmail.com>
2022-05-09 22:16:02 -0500

    adding map function

--- src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -7,16 +7,28 @@ import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TransformationsMapAndReduce {
 
     @Test
     void yourFirstTransformationWithMap() throws IOException {
         List<Person> people = MockData.getPeople();
+        Function<Person, PersonDTO> personToPersonDTO = person ->
+            new PersonDTO(
+                    person.getId(),
+                    person.getFirstName(),
+                    person.getAge());
+
+        List<PersonDTO> dtos = people.stream()
+                .filter(person -> person.getAge().equals(20))
+                .map(personToPersonDTO).collect(Collectors.toList());
+//        assertThat(people.size()).isEqualTo(dtos.size());
+        dtos.forEach(System.out::println);
     }
 
     @Test
